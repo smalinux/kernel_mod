56cbb39d9d9113b273eb3fb3af37eb8b07894ed9
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2020-05-13 07:29:27 +0200

    eudyptula

--- sma_fs.c
+++ sma_fs.c
@@ -1,12 +1,13 @@
 /*  
- *	
+ *	Linux virtual filesystems
+ *	Ref: https://lwn.net/Articles/57369/
  */
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 
 static int __init sma_hello(void)
 {
-	printk(KERN_ERR "Hello world 2.\n");
+	printk(KERN_ERR "Hello world\n");
 
 
 	return 0;
@@ -14,7 +15,7 @@ static int __init sma_hello(void)
 
 static void __exit sma_bye(void)
 {
-	printk(KERN_INFO "Goodbye world 2.\n");
+	printk(KERN_INFO "Goodbye world\n");
 }
 module_init(sma_hello);
 module_exit(sma_bye);
